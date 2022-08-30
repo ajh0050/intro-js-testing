25e752edeb946b0e551b68a9d51d49653730a544
Adam Hughes <adamjordanhughes@gmail.com>
2022-08-30 16:40:22 -0400

    Add tweet functionality

--- tweet.js
+++ tweet.js
@@ -1,5 +1,23 @@
 class Tweet {
-
+  constructor(sender,message,link='no link provided',likes=0,retweets=0,replies=[],replyCount=0){
+    this.sender = sender
+    this.content = message
+    this.link = link
+    this.likes  = likes
+    this.retweets = retweets
+    this.replies = replies
+    this.replyCount = replyCount
+  }
+  like(){
+    this.likes++
+  }
+  retweet(){
+    this.retweets++
+  }
+  reply(replyMessage){
+    this.replies.push(replyMessage)
+    this.replyCount++
+  }
 }
 
 module.exports = Tweet;
